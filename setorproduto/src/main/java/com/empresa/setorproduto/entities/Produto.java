53cf3aeb33920eaee09650e0e1aee1820a1771b3
SARA MACHADO FERNANDES <sara_m_fernandes@estudante.sesisenai.org.br>
2025-03-26 22:00:54 -0300

    commit debug

--- setorproduto/src/main/java/com/empresa/setorproduto/entities/Produto.java
+++ setorproduto/src/main/java/com/empresa/setorproduto/entities/Produto.java
@@ -1,15 +1,20 @@
 package com.empresa.setorproduto.entities;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
+@Entity
 public class Produto {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nome;
+
+    @ManyToOne
     private Setor setor;
 
     public int getId() {
