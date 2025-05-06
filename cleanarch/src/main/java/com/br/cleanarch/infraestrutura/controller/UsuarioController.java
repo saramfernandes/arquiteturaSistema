b6a17c8310cd8727767bc0c3ed38071556cbb303
SARA MACHADO FERNANDES <sara_m_fernandes@estudante.sesisenai.org.br>
2025-05-05 21:42:25 -0300

    aula de cleanarch

--- cleanarch/src/main/java/com/br/cleanarch/infraestrutura/controller/UsuarioController.java
+++ cleanarch/src/main/java/com/br/cleanarch/infraestrutura/controller/UsuarioController.java
@@ -1,6 +1,11 @@
 package com.br.cleanarch.infraestrutura.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +26,12 @@ public class UsuarioController {
     @PostMapping("/novo")
     public UsuarioResponse salvar(@RequestBody UsuarioRequest request) {
         return mapper.toResponse(interactor.createUsuario(mapper.toDomain(request)));
+    }
+
+    @GetMapping("/lista")
+    public List<UsuarioResponse> lista() {
+        List<UsuarioResponse> listas = interactor.listaUsuario().stream().map(mapper::toResponse).collect(Collectors.toList());
+        return listas;
     }
 
 }
